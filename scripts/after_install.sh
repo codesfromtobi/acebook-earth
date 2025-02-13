267b14d00b2daadb93f5f8ec746cae0e44e0ce5d
olluwatobi.55@gmail.com <X@MacBook-Pro.local>
2025-02-13 07:01:36 -0800

    updated deployment files and removed scripts folder

--- scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Navigate to application directory
-cd /var/www/html
-# Install application dependencies
-npm install
-# Start the application
-pm2 start app.js
--- scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Install dependencies
-sudo yum update -y
-sudo yum install -y nodejs npm
